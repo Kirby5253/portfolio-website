0e0acb9e3e7b57505acdfd6f0638766ee95ec0fd
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-07-30 09:37:07 -0500

    fixed bug issue with contact form

--- js/index.js
+++ js/index.js
@@ -7,3 +7,24 @@ $('#gform').on('submit', function(e) {
   });
   $('#gform').prepend('Your submission has been processed...');
 });
+
+var message = '';
+
+$('#sendMessage').on('click', function() {
+  message = $('#gform').serialize().split('&');
+  $.ajax({
+    url: 'https://formspree.io/mdowbnpk',
+    method: 'POST',
+    data: { message: message },
+    dataType: 'json',
+  });
+});
+
+function validateForm() {
+  let name = $('#contact-name');
+  let email = $('#contact-email');
+  let message = $('#contact-message');
+  let submitButton = $('#sendMessage');
+
+  name && email && message ? submitButton.removeClass('disabled') : null;
+}
