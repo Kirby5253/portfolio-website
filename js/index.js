cc9042a64efe38897f9f6a07881a0d006a1aaff7
Jacob Kirby <65231998+Kirby5253@users.noreply.github.com>
2020-08-17 13:09:46 -0500

    added serverless functionality to form

--- js/index.js
+++ js/index.js
@@ -1,24 +1,14 @@
 // handles contact form submission
 var submitted = false;
-$('#gform').on('submit', function(e) {
-  e.preventDefault();
-  $('#gform *').slideUp(500, function() {
-    $(this).remove();
-  });
-  $('#gform').prepend('Your submission has been processed...');
-});
+// $('#gform').on('submit', function(e) {
+//   e.preventDefault();
+//   $('#gform *').slideUp(500, function() {
+//     $(this).remove();
+//   });
+//   $('#gform').prepend('Thanks so much for your submission!');
+// });
 
 var message = '';
-
-$('#sendMessage').on('click', function() {
-  message = $('#gform').serialize().split('&');
-  $.ajax({
-    url: 'https://formspree.io/mdowbnpk',
-    method: 'POST',
-    data: { message: message },
-    dataType: 'json',
-  });
-});
 
 function validateForm() {
   let name = $('#contact-name');
@@ -34,3 +24,47 @@ $(document).ready(function() {
     interval: 3000,
   });
 });
+
+(() => {
+  const form = $('#gform');
+
+  form.on('submit', function(e) {
+    e.preventDefault();
+
+    // Prepare data to send
+    const data = {
+      name: $('#contact-name').val(),
+      reply_to: $('#contact-email').val(),
+      message: $('#contact-message').val(),
+    };
+
+    // Log what our lambda function will receive
+    console.log(JSON.stringify(data));
+
+    // Construct an HTTP request
+    var xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+    // Send the collected data as JSON
+    xhr.send(JSON.stringify(data));
+
+    xhr.onloadend = (response) => {
+      if (response.target.status === 200) {
+        // The form submission was successful
+        $('#gform *').slideUp(500, function() {
+          $(this).remove();
+        });
+        $('#gform').prepend("Thanks so much for your submission! I'll be in touch shortly.");
+      } else {
+        // The form submission failed
+        $('#gform *').slideUp(500, function() {
+          $(this).remove();
+        });
+        $('#gform').prepend('Something went wrong, please use the contact links...');
+        console.error(JSON.parse(response.target.response).message);
+      }
+    };
+  });
+})();
